310a78fe103a7035709d3968faf008ae91b63778
tomas.mccandless <tomas.mccandless@workday.com>
2019-03-20 23:55:08 -0700

    zipper

---
+++ src/main/scala/org/mccandless/coolshit/zipper/Zipper.scala
@@ -0,0 +1,68 @@
+package org.mccandless.coolshit.zipper
+
+/**
+  *
+  * data Cxt a = Top | L (Cxt a) (Tree a) | R (Tree a) (Cxt a)
+  *
+  * Represents path from subtree to the root (rather than the other way round)
+  *
+  * focus of a subtree together with a context
+  * Created by tomas.mccandless on 2019-03-20.
+  */
+sealed trait Context[+A]
+
+// root of the tree
+case object Top extends Context[Nothing]
+// left part of a branch of which the right part was t and whose parent had context c
+case class Left[A](cxt: Context[A], tree: Tree[A]) extends Context[A]
+case class Right[A](tree: Tree[A], cxt: Context[A]) extends Context[A]
+
+
+case class Zipper[A](focus: Tree[A], context: Context[A]) {
+
+  // (Fork l r, c) = (l, L c r)
+  def left: Zipper[A] = this match {
+    case Zipper(Branch(l, r), c) => Zipper(l, Left(c, r))
+    case _ => ???
+  }
+
+
+  // (Fork l r, c) = (r, R l c)
+  def right: Zipper[A] = this match {
+    case Zipper(Branch(l, r), c) => Zipper(r, Right(l, c))
+    case _ => ???
+  }
+
+
+  //up (t, L c r) = (Fork t r, c)
+  //up (t, R l c) = (Fork l t, c)
+  def up: Zipper[A] = this match {
+    case Zipper(t, Left(c, r)) => Zipper(Branch(t, r), c)
+    case Zipper(t, Right(l, c)) => Zipper(Branch(l, t), c)
+    case _ => ???
+  }
+
+
+  // upmost :: Loc a -> Loc a
+  // upmost l@(t, Top) = l
+  // upmost l = upmost (up l)
+  def upmost: Zipper[A] = this match {
+    case Zipper(_, Top) => this
+    case z => z.up.upmost
+  }
+
+
+  // modify :: Loc a -> (Tree a -> Tree a) -> Loc a
+  // modify (t, c) f = (f t, c)
+  def modify(f: Tree[A] => Tree[A]): Zipper[A] = {
+    this.copy(focus = f(this.focus))
+  }
+}
+
+
+object Zipper {
+
+  def apply[A](t: Tree[A]): Zipper[A] = Zipper(t, Top)
+}
+
+
---
+++ src/test/scala/org/mccandless/coolshit/zipper/ZipperSpec.scala
@@ -0,0 +1,45 @@
+package org.mccandless.coolshit.zipper
+
+import org.mccandless.coolshit.BaseSpec
+
+/**
+  * Created by tomas.mccandless on 2019-03-20.
+  */
+class ZipperSpec extends BaseSpec{
+
+  "zipper" should "zip" in {
+    // how can we modify this?
+    val tree = Branch(
+      left = Branch(
+        left = Branch(Leaf(1), Leaf(2)),
+        right = Branch(Leaf(3), Leaf(4))
+      ),
+      right = Branch(
+        left = Branch(Leaf(5), Leaf(6)),
+        right = Branch(Leaf(7), Leaf(8))
+      )
+    )
+
+    // all this .copy seems ugly
+//    val tPrime = t.copy(left = t.left.copy(right = Leaf(3)))
+
+
+    // create a zipper, this uses context Top to signify root of tree
+    val z: Zipper[Int] = Zipper(tree)
+    // simple sanity check
+    z.left.left.right.focus should be (Leaf(2))
+
+    // traverse and modify. common subtrees unmodified are shared in memory, similar to copy on write
+    // in general, zipper allows for elements close to the cursor to be efficiently updated
+    val t2 = z.left.left.right.modify(_ => Leaf(45)).upmost.focus
+    // we could also graft in an entirely different subtree here
+//    val t2 = z.left.left.right.modify(_ => Branch(Leaf(1), Leaf(2))).upmost.focus
+
+    // "reify" the tree and check that our update took
+    Zipper(t2).left.left.right.focus should be (Leaf(45))
+    println(t2)
+
+    // should be able to recover the original tree unmodified
+    z.left.left.upmost.focus should be (tree)
+  }
+}
